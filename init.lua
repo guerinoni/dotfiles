3e3148c9c6265e0441f115d6a78a79bbcc4012f1
Federico Guerinoni <guerinoni.federico@gmail.com>
2022-12-22 16:21:39 +0100

    Add generic grep on files
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- init.lua
+++ init.lua
@@ -198,6 +198,9 @@ vim.keymap.set(
 )
 
 vim.keymap.set("n", "<leader>sf", require("telescope.builtin").find_files, {desc = "[S]earch [F]iles"})
+vim.keymap.set('n', '<leader>sw', function()
+	require("telescope.builtin").grep_string({ search = vim.fn.input("Grep > ") })
+end)
 -- vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 -- vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 -- vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
