87a64c867caf8a3a54e0d6f178c2e6b046e14d43
Federico Guerinoni <guerinoni.federico@gmail.com>
2022-12-23 10:21:08 +0100

    Add script for automatic install
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

---
+++ install.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+# -e exit on error
+# -u error on undefined variables
+# -o option
+# pipefail exits on command pipe failures
+set -euo pipefail
+
+echo "installing dotfiles..."
+
+# nvim
+mkdir -p "$HOME/.config/nvim"
+ln -s "$(pwd)/config/nvim/init.lua" "$HOME/.config/nvim/init.lua"
+
+echo "finished :)"
+
