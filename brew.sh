f6a1d72c7ca0d93a5c9749f7ecc8e58400c35c96
Federico Guerinoni <guerinoni.federico@gmail.com>
2022-12-25 02:12:42 +0100

    brew: Add default app to install
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

---
+++ brew.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+
+/bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> "$HOME/.zprofile"
+
+brew install ripgrep
