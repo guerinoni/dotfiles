499748ccc338233679e0da914d507399430a88df
Federico Guerinoni <guerinoni.federico@gmail.com>
2022-12-28 15:07:17 +0100

    brew: Add `delta` for better git experience
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- brew.sh
+++ brew.sh
@@ -3,4 +3,4 @@
 /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> "$HOME/.zprofile"
 
-brew install ripgrep
+brew install ripgrep git-delta
