959b3fc1c7effb307825db98451440c6a411b8e0
Federico Guerinoni <guerinoni.federico@gmail.com>
2023-01-12 21:08:51 +0100

    brew: Add `graphviz` as default installed
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- brew.sh
+++ brew.sh
@@ -3,6 +3,6 @@
 /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> "$HOME/.zprofile"
 
-brew install ripgrep git-delta wget
+brew install ripgrep git-delta wget graphviz
 brew tap homebrew/cask-fonts
 brew install --cask font-iosevka
