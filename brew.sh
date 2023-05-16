a3290ee9e0a2ef78a53c9fc885749cce7c30e6cd
Federico Guerinoni <guerinoni.federico@gmail.com>
2023-05-16 10:34:09 +0200

    brew: Add `watch` to default install
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- brew.sh
+++ brew.sh
@@ -3,7 +3,7 @@
 /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> "$HOME/.zprofile"
 
-brew install ripgrep git-delta wget graphviz jq cmake
+brew install ripgrep git-delta wget graphviz jq cmake watch
 brew tap homebrew/cask-fonts
 brew install --cask font-iosevka
 brew install --cask browserosaurus # browser selector on link click :)
