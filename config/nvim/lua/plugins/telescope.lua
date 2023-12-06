3840419701be263ee195de5ee465d42c4a815037
Federico Guerinoni <guerinoni.federico@gmail.com>
2023-12-06 16:22:33 +0100

    nvim: Simply config
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,10 +1,7 @@
 return {
-    'nvim-telescope/telescope.nvim',
+    'nvim-telescope/telescope.nvim', branch = '0.1.x',
     dependencies = {
       'nvim-lua/plenary.nvim',
-      'nvim-tree/nvim-web-devicons',
-      'nvim-telescope/telescope-live-grep-args.nvim',
-      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     },
     keys = {
       { '<leader>f', function() require('telescope.builtin').git_files() end },
@@ -15,64 +12,6 @@ return {
       { '<leader>s', function() require('telescope.builtin').lsp_document_symbols() end },
     },
     config = function ()
-      local actions = require('telescope.actions')
-  
-      require('telescope').setup({
-        defaults = {
-          path_display = { truncate = 1 },
-          prompt_prefix = '   ',
-          selection_caret = '  ',
-          layout_config = {
-            prompt_position = 'top',
-          },
-          preview = {
-            timeout = 200,
-          },
-          sorting_strategy = 'ascending',
-          mappings = {
-            i = {
-              ['<esc>'] = actions.close,
-              ['<C-Down>'] = actions.cycle_history_next,
-              ['<C-Up>'] = actions.cycle_history_prev,
-            },
-          },
-          file_ignore_patterns = { '.git/' },
-        },
-        extensions = {
-          live_grep_args = {
-            mappings = {
-              i = {
-                ["<C-k>"] = require("telescope-live-grep-args.actions").quote_prompt(),
-                ["<C-i>"] = require("telescope-live-grep-args.actions").quote_prompt({ postfix = " --iglob " }),
-              },
-            },
-          },
-        },
-        pickers = {
-          find_files = {
-            hidden = true,
-          },
-          buffers = {
-            previewer = false,
-            layout_config = {
-              width = 80,
-            },
-          },
-          oldfiles = {
-            prompt_title = 'History',
-          },
-          lsp_references = {
-            previewer = false,
-          },
-          lsp_definitions = {
-            previewer = false,
-          },
-          lsp_document_symbols = {
-            symbol_width = 55,
-          },
-        },
-      })
-  
-      require('telescope').load_extension('fzf')
+      require('telescope')
     end,
   }
--- config/nvim/lua/plugins/tokyonight.lua
+++ config/nvim/lua/plugins/tokyonight.lua
@@ -1,11 +1,6 @@
 return {
     'folke/tokyonight.nvim',
-    lazy = false,
-    priority = 1000,
-    opts = {},
-    config = function (plugin, opts)
-      require('tokyonight').setup(opts)
-  
-      vim.cmd('colorscheme tokyonight')
-    end,
+    config = function()
+      require("tokyonight").colorscheme()
+    end
   }
