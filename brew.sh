e39934a50e90f3dbb2655ee5347267f249044518
Federico Guerinoni <guerinoni.federico@gmail.com>
2024-01-27 11:22:34 +0100

    brew: Add more used tool
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- brew.sh
+++ brew.sh
@@ -4,11 +4,15 @@
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> "$HOME/.zprofile"
 
 brew install docker git-delta wget gh nvim ripgrep libpq
+
 brew tap homebrew/cask-fonts
 brew install --cask font-iosevka
+
 brew install --cask browserosaurus # browser selector on link click :)
 brew install --cask wezterm
 brew install --cask rectangle
 brew install --cask arc
+brew install --cast bitwarden
+brew install --cask maccy
 
 brew cleanup
