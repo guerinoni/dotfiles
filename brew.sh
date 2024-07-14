3b4aa7db8e01b7f091b008eec02cae2a6a5ff3c5
Federico Guerinoni <guerinoni.federico@gmail.com>
2024-07-14 10:47:55 +0200

    brew: Remove deprecated tap

--- brew.sh
+++ brew.sh
@@ -5,7 +5,6 @@ echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> "$HOME/.zprofile"
 
 brew install gpg2 orbstack git-delta wget gh nvim ripgrep libpq xsv atuin direnv
 
-brew tap homebrew/cask-fonts
 brew install --cask font-iosevka
 
 brew install --cask browserosaurus # browser selector on link click :)
