c3723038cbf3377320d610358b5dad04b82b1107
Federico Guerinoni <guerinoni.federico@gmail.com>
2024-11-29 11:14:52 +0100

    nvim: Remove copilot

--- config/nvim/lua/custom/plugins.lua
+++ config/nvim/lua/custom/plugins.lua
@@ -22,32 +22,6 @@ local plugins = {
     end,
   },
   {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    build = ":Copilot auth",
-    event = "InsertEnter",
-    opts = {
-      suggestion = {
-        enabled = true,
-        auto_trigger = true,
-        keymap = {
-          accept = "<Tab>",
-          close = "<Esc>",
-        },
-      },
-      panel = {
-        enabled = false,
-      },
-    },
-  },
-  {
-    "zbirenbaum/copilot-cmp",
-    dependencies = {
-      "hrsh7th/nvim-cmp",
-    },
-    config = true,
-  },
-  {
     "rust-lang/rust.vim",
     ft = "rust",
     init = function()
