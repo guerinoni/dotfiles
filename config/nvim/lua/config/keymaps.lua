67198a7c9f218537dbfb21b3b22159566f879a8c
Federico Guerinoni <guerinoni.federico@gmail.com>
2025-12-04 10:45:37 +0100

    nvim: add shortcut for diagnostics problems

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -6,3 +6,18 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Easy save
 vim.keymap.set("n", "<leader>w", "<cmd>w<CR>", { desc = "Save File" })
+
+-- Quickfix list for all diagnostics (LSP-wide, not just file)
+vim.keymap.set('n', '<leader>do', function()
+  vim.diagnostic.setqflist()
+  vim.cmd('copen')
+end, { desc = "Open quickfix list with all diagnostics" })
+
+vim.keymap.set('n', '<leader>dc', function()
+  vim.cmd('cclose')
+end, { desc = "Close quickfix list" })
+
+-- Telescope workspace diagnostics (if you have telescope.nvim)
+vim.keymap.set('n', '<leader>dw', function()
+  require('telescope.builtin').diagnostics({ bufnr = nil })
+end, { desc = "Workspace diagnostics (Telescope)" })
---
+++ config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,7 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    require("telescope").setup()
+  end
+}
