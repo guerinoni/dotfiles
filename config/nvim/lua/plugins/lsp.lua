1713fd723483e929e1777115ac52275c024aa7ee
Federico Guerinoni <guerinoni.federico@gmail.com>
2026-05-16 15:21:30 +0200

    nvim: drop duplicate local client declaration in LspAttach
    The same vim.lsp.get_client_by_id(event.data.client_id) was assigned
    to `client` twice in the same scope. Removed the second one; the
    first declaration at line 48 is still in scope for the rest of the
    callback.

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -69,7 +69,6 @@ return {
                 --    See `:help CursorHold` for information about when this is executed
                 --
                 -- When you move your cursor, the highlights will be cleared (the second autocommand).
-                local client = vim.lsp.get_client_by_id(event.data.client_id)
                 if
                     client and
                     client_supports_method(
