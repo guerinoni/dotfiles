def848d455d0a219b5b6f44ccc5469417ebced9e
Federico Guerinoni <guerinoni.federico@gmail.com>
2026-07-27 15:43:11 +0200

    nvim: drop no-op completion option
    vim.b.completion was never a real option, so the line did nothing beyond
    setting a stray buffer variable on whatever buffer happened to exist at
    startup. Completion comes from blink.cmp anyway.

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -13,7 +13,6 @@ vim.o.undofile = true           -- Save undo history
 vim.o.showmode = false          -- Hide mode information, it is in status line
 vim.o.signcolumn = "yes"        -- Show sign column
 vim.o.clipboard = "unnamedplus" -- Enable clipboard integration with system clipboard
-vim.b.completion = true         -- Enable completion
 
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  Notice listchars is set using `vim.opt` instead of `vim.o`.
