87eccd141de58b600c84dc46e8893642a499fa6d
Federico Guerinoni <guerinoni.federico@gmail.com>
2026-08-05 12:01:10 +0200

    install: avoid upgrading packages during install

--- install.sh
+++ install.sh
@@ -56,7 +56,7 @@ fi
 
 log "Running brew bundle..."
 if [[ -f "$SCRIPT_DIR/Brewfile" ]]; then
-  brew bundle install --file="$SCRIPT_DIR/Brewfile"
+  brew bundle install --no-upgrade --file="$SCRIPT_DIR/Brewfile"
 else
   log "Skipped: Brewfile not found"
 fi
